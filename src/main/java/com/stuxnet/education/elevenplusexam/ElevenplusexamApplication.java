f14a582054023a7e86642429a0a323bd909fe993
Pankaj Pardasani <pankaj.d.p@me.com>
2017-05-18 06:52:30 +0100

    initial project files

---
+++ src/main/java/com/stuxnet/education/elevenplusexam/ElevenplusexamApplication.java
@@ -0,0 +1,12 @@
+package com.stuxnet.education.elevenplusexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevenplusexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevenplusexamApplication.class, args);
+	}
+}
